5e6df8e6704f4358df1a9acf2ebfad2bafe9e38d
kriskrystev <krystev.kristiyan@gmail.com>
2023-12-11 00:51:19 +0200

    Add reset button for testing puproses handle line redrawing and keeping old objects on screen while doing so

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 import getCursorPosition from "./cursor-helper.js";
 import { initShapesClickListenersWith, initShapes } from "./shape-listeners.js";
+import { LineBuilder } from "./shapes/line.js";
 import { RectangleBuilder } from "./shapes/rectangle.js";
 
 (function () {
   let shapeStroke = "red";
+  const leftPanelWidth = 200;
+  const headerHeight = 30;
+
+  const resetButton = document.querySelector(".reset-button");
   const canvas = document.getElementById("canvas");
   const context = canvas.getContext("2d");
   const focusClass = "shapes__element--focus";
 
-  canvas.width = window.innerWidth - 200;
-  canvas.height = window.innerHeight - 30;
+  canvas.width = window.innerWidth - leftPanelWidth;
+  canvas.height = window.innerHeight - headerHeight;
+
   const { peekShape, selectShape, shapeTypes } = initShapes();
+  resetButton.addEventListener("click", reset);
   const listeners = initShapesClickListenersWith(shapeTypes, function (shape) {
     const prevShape = peekShape();
 
@@ -20,31 +27,68 @@ import { RectangleBuilder } from "./shapes/rectangle.js";
     shape.classList.add(focusClass);
     selectShape(shape);
   });
+  let drag = false;
+  let currentlyDrawnShape = null;
+  const lineBuilder = new LineBuilder();
+  const layers = [
+    {
+      name: "default",
+      shapes: [],
+    },
+  ];
+
+  let animationFrameId = null;
 
   // handlers for start and stop draw
   canvas.addEventListener("mousedown", (event) => {
+    animationFrameId = window.requestAnimationFrame(loop);
+
     if (peekShape() && peekShape().dataset.shapeType === "line") {
       const { x, y } = getCursorPosition(canvas, event);
-      context.beginPath();
-      context.moveTo(x, y);
+      currentlyDrawnShape = lineBuilder.setStart(x, y).setEnd(x, y).build();
+
+      drag = true;
+    }
+  });
+
+  canvas.addEventListener("mousemove", (event) => {
+    if (drag) {
+      const { x, y } = getCursorPosition(canvas, event);
+      currentlyDrawnShape.setEnd(x, y);
     }
   });
 
   canvas.addEventListener("mouseup", (event) => {
     if (peekShape() && peekShape().dataset.shapeType === "line") {
       const { x, y } = getCursorPosition(canvas, event);
-      context.lineTo(x, y);
-      context.strokeStyle = shapeStroke;
-      context.stroke();
+      currentlyDrawnShape.setEnd(x, y);
+      layers[0].shapes.push(currentlyDrawnShape);
+      currentlyDrawnShape = null;
     }
+    drag = false;
+    if (animationFrameId) window.cancelAnimationFrame(animationFrameId);
   });
 
-  const rectangleBuilder = new RectangleBuilder();
-  const rectangle = rectangleBuilder
-    .setX(100)
-    .setY(100)
-    .setWidth(200)
-    .setHeight(200)
-    .build();
-  rectangle.draw(context);
+  canvas.addEventListener("mouseout", (event) => {
+    if (animationFrameId) window.cancelAnimationFrame(animationFrameId);
+    drag = false;
+  });
+
+  function loop() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    animationFrameId = window.requestAnimationFrame(loop);
+
+    if (drag) {
+      if (currentlyDrawnShape) {
+        currentlyDrawnShape.draw(context);
+        layers[0].shapes.forEach((shape) => shape.draw(context));
+      }
+    }
+  }
+
+  function reset() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    window.cancelAnimationFrame(animationFrameId);
+    layers[0].shapes = [];
+  }
 })();
--- shapes/line.js
+++ shapes/line.js
@@ -1,23 +1,38 @@
 function Line(x1, y1, x2, y2, strokeStyle = "black") {
   this.x1 = x1;
-  this.x2 = x2;
   this.y1 = y1;
+
+  this.x2 = x2;
   this.y2 = y2;
   this.strokeStyle = strokeStyle;
 
+  this.setStart = function (x, y) {
+    this.x1 = x;
+    this.y1 = y;
+    return this;
+  };
+
+  this.setEnd = function (x, y) {
+    this.x2 = x;
+    this.y2 = y;
+    return this;
+  };
+
   this.draw = function (context) {
     context.beginPath();
-    context.moveTo(x1, y1);
-    context.lineTo(x2, y2);
+    context.moveTo(this.x1, this.y1);
+    context.lineTo(this.x2, this.y2);
     context.strokeStyle = this.strokeStyle;
     context.stroke();
+    context.closePath();
   };
 }
 
 export function LineBuilder() {
   this.x1 = 0;
-  this.x2 = 0;
   this.y1 = 0;
+
+  this.x2 = 0;
   this.y2 = 0;
   this.strokeStyle = "black";
 
